26dea4441cf3f0f31375b217bfa8862c6457c478
Jonathan Silva <jonalves.silva@jsmail.com.br>
2025-06-13 11:15:08 -0300

    feat: add div Search and animation to Header component

--- frontend/components/custom/Header/Header.tsx
+++ frontend/components/custom/Header/Header.tsx
@@ -1,26 +1,82 @@
+"use client";
 import { IoMdAirplane } from "react-icons/io";
 import ProfileButtons from "./ProfileButtons";
 import MobileMenu from "./MobileMenu";
 import LinksCardsMenu from "./LinksCardsMenu";
+import { useState, useEffect, useRef } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import "animate.css";
 
 export default function Header() {
+  const [showSearchDiv, setShowSearchDiv] = useState(false);
+  const [renderSearchDiv, setRenderSearchDiv] = useState(false);
+  const searchRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (showSearchDiv) {
+      setRenderSearchDiv(true);
+    } else {
+      const timeout = setTimeout(() => setRenderSearchDiv(false), 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [showSearchDiv]);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        searchRef.current &&
+        !searchRef.current.contains(event.target as Node)
+      ) {
+        setShowSearchDiv(false);
+      }
+    }
+
+    if (renderSearchDiv) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [renderSearchDiv]);
+
   return (
-    <header className="h-26 w-full fixed">
-      <div className="bg-maroon h-[45%] flex items-center justify-center gap-x-3 px-2">
+    <header className="h-26 fixed w-full">
+      <div className="bg-maroon flex h-[45%] items-center justify-center gap-x-3 px-2">
         <IoMdAirplane size={20} className="text-white" />
-        <span className="text-xs text-white text-center font-semibold">
+        <span className="text-center text-xs font-semibold text-white">
           FRETE GRÁTIS PARA TODO O BRASIL NAS COMPRAS ACIMA DE R$ 499,99
         </span>
         <IoMdAirplane size={20} className="text-white" />
       </div>
       <div className="bg-green h-[55%] border border-white">
-        <nav className="w-full h-full flex items-center justify-between max-w-screen-xl mx-auto px-12 lg:px-6">
+        <nav className="mx-auto flex h-full w-full max-w-screen-xl items-center justify-between px-12 lg:px-6">
           <img src="logo.png" className="w-[145px]" />
           <LinksCardsMenu />
-          <ProfileButtons />
+          <ProfileButtons search={{ setShowSearchDiv, showSearchDiv }} />
           <MobileMenu />
         </nav>
       </div>
+
+      {renderSearchDiv && (
+        <div
+          ref={searchRef}
+          className={`animate__animated flex h-14 items-center border-b border-gray-300 shadow-sm shadow-neutral-300 ${
+            showSearchDiv ? "animate__fadeIn" : "animate__fadeOut"
+          }`}
+        >
+          <div className="mx-auto flex w-full max-w-screen-lg items-center justify-between gap-x-12">
+            <Input
+              className="!border-gray-400/70"
+              placeholder="Digite aqui o que deseja buscar"
+            />
+            <Button className="bg-green hover:bg-green cursor-pointer">
+              Buscar
+            </Button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
--- frontend/components/custom/Header/ProfileButtons.tsx
+++ frontend/components/custom/Header/ProfileButtons.tsx
@@ -2,10 +2,22 @@ import { FaUser } from "react-icons/fa";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoMdCart } from "react-icons/io";
 
-export default function ProfileButtons() {
+interface Props {
+  search: {
+    setShowSearchDiv: (showDivSearch: boolean) => void;
+    showSearchDiv: boolean;
+  };
+}
+
+export default function ProfileButtons({ search }: Props) {
+  const { setShowSearchDiv, showSearchDiv } = search;
   return (
-    <div className="hidden lg:flex items-center gap-x-3">
-      <IoSearchOutline size={20} className="text-white" />
+    <div className="hidden items-center gap-x-3 lg:flex">
+      <IoSearchOutline
+        size={20}
+        className="cursor-pointer text-white"
+        onClick={() => setShowSearchDiv(!showSearchDiv)}
+      />
       <IoMdCart size={20} className="text-white" />
       <FaUser size={20} className="text-white" />
     </div>
