66a4b3b787237b164c11a6431696fc344716d363
Jonathan Silva <jonalves.silva@jsmail.com.br>
2025-06-13 15:06:02 -0300

    feat: modify MobileMenu component and add accordionMenus

--- frontend/components/custom/Header/MobileMenu.tsx
+++ frontend/components/custom/Header/MobileMenu.tsx
@@ -1,27 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Sheet,
   SheetTrigger,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
+  SheetFooter,
 } from "@/components/ui/sheet";
 import { IoMenu } from "react-icons/io5";
+import MobileMenuAccordion from "./MobileMenuAccordion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function MobileMenu() {
   return (
     <Sheet>
       <SheetTrigger className="lg:hidden">
-        <IoMenu size={20} className=" text-white cursor-pointer" />
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+        <IoMenu size={20} className="cursor-pointer text-white" />
+      </SheetTrigger>{" "}
+      <SheetContent className="bg-green !gap-0 overflow-y-auto !border-none">
+        <SheetHeader className="border-b border-gray-50/30">
+          <SheetTitle>
+            <img src="logo.png" className="w-[150px]" />
+          </SheetTitle>
         </SheetHeader>
+        {/*Div Search*/}
+        <div className="flex w-full gap-x-2 border-b border-gray-50/30 px-2 py-4">
+          <Input
+            className="!border-gray-400/70 bg-white"
+            placeholder="Buscar"
+          />
+          <Button className="bg-maroon hover:bg-green cursor-pointer">
+            Buscar
+          </Button>
+        </div>
+
+        {/*Menu Accordion*/}
+        <MobileMenuAccordion />
+        <SheetFooter className="border-t border-gray-50/30">teste</SheetFooter>
       </SheetContent>
     </Sheet>
   );
---
+++ frontend/components/custom/Header/MobileMenuAccordion.tsx
@@ -0,0 +1,35 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { linksCardMenu } from "@/constants/linksCardsMenu";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function MobileMenuAccordion() {
+  return (
+    <ScrollArea className="flex-1">
+      <Accordion type="single" collapsible>
+        {linksCardMenu.map((item: any, index: number) => (
+          <AccordionItem
+            key={`accordion_${item.menu}`}
+            value={`item-${index}`}
+            className="border-b border-gray-50/30 px-2 text-white [&_svg]:text-white"
+          >
+            <AccordionTrigger>{item.menu}</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                {item.submenu.map((subItem: any) => (
+                  <li key={subItem.menu} className="py-2 pl-4">
+                    {subItem.menu}
+                  </li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </ScrollArea>
+  );
+}
