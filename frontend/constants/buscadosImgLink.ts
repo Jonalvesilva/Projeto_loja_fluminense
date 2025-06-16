0378b096b8a08d85bbc883ee48c42f5bf6296c6e
Jonathan Silva <jonalves.silva@jsmail.com.br>
2025-06-16 15:01:14 -0300

    feat: add constant buscadosImgLink and images files

---
+++ frontend/constants/buscadosImgLink.ts
@@ -0,0 +1,8 @@
+export const buscadosImgLink = [
+  { name: "Masculino", src: "/maisBuscados/masculino.png" },
+  { name: "Feminino", src: "/maisBuscados/feminino.png" },
+  { name: "Copos", src: "/maisBuscados/copos.png" },
+  { name: "Libertadores", src: "/maisBuscados/campeoes.png" },
+  { name: "Calçados", src: "/maisBuscados/calcado.png" },
+  { name: "Armaduras", src: "/maisBuscados/armadura.png" },
+];
