7f6d9275a907cf3c542f16a39dcde3c8431f270e
Jonathan Silva <jonalves.silva@jsmail.com.br>
2025-06-25 10:37:36 -0300

    feat: add WhatsappBottomButton component

---
+++ frontend/components/custom/WhatsappBottomButton/WhatsappBottomButton.tsx
@@ -0,0 +1,7 @@
+export default function WhatsappBottomButton() {
+  return (
+    <div className="fixed bottom-3 right-3 z-20">
+      <img src="/utils/whatsapp.png" className="w-14 animate-pulse" />
+    </div>
+  );
+}
